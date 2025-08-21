0a841fd7b63ac67662f6b3644a17ff030d574072
Sunil Utagi <sunilutagi7@gmail.com>
2025-08-21 12:53:18 +0530

    Add files via upload

---
+++ Test/Test1/Q1.py
@@ -0,0 +1,11 @@
+length = int(input("Enter the length : "))
+breadth = int(input("Enter the breadth : "))
+
+#area
+area = length * breadth
+
+#perimeter
+perimeter = 2 * (length + breadth)
+
+print(f"Area is : {area}")
+print(f"Perimeter is : {perimeter}")
