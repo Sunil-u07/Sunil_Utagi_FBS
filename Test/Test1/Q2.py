77e8e0a2ba1d9aae211d7810b9a1321a08280b27
Sunil Utagi <sunilutagi7@gmail.com>
2025-08-21 10:54:14 +0530

    Add files via upload

---
+++ Test/Test1/Q2.py
@@ -0,0 +1,7 @@
+#Q2
+P=int(input('Enter principle:'))
+R=int(input('Enter rate:'))
+T=int(input('Enter time:'))
+
+SI=(P*R*T)/100
+print('Simple Interest is:',SI)
---
+++ Test/Test1/Q3.py
@@ -0,0 +1,10 @@
+km = int(input("Enter distance in kilometers: "))
+
+#meters
+meters = km * 1000
+
+#centimeters
+centimeters = km * 100000
+
+print(f"Distance in meters: {meters}")
+print(f"Distance in centimeters: {centimeters}")
