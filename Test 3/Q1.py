402638fc8a135c0833c9f47eafa1162a645177ae
Sunil Utagi <sunilutagi7@gmail.com>
2025-09-08 01:02:54 -0700

    Add files via upload

---
+++ Test 3/Q1.py
@@ -0,0 +1,15 @@
+n = int(input("Enter how many prime numbers you want: "))
+print("The first", n, "prime numbers are:")
+
+count = 0
+num = 2
+
+while count < n:
+
+    for i in range(2, num):
+        if num % i == 0:
+            break
+    else:
+        print(num, end=" ")
+        count += 1
+    num += 1
