ed54bb8adebe3a50d5e57bd89a2af5d571951864
Sunil Utagi <sunilutagi7@gmail.com>
2025-09-11 19:57:17 +0530

    Add files via upload

---
+++ Assignment 7/d.py
@@ -0,0 +1,15 @@
+#Number Pyramid Pattern
+n = 5
+for i in range(1, n+1):
+    # Spaces
+    print("  " * (n-i), end="")
+
+    # Increasing 
+    for j in range(i, 2*i):
+        print(j, end="")
+        print(" ", end="")
+
+    # Decreasing 
+    for j in range(2*i-2, i-1, -1):
+        print(j, end=" ")
+    print()
