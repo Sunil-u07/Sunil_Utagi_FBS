c4a4646a01e17abd50616bc5bd5e0b2cdca2dd79
Sunil Utagi <sunilutagi7@gmail.com>
2025-09-25 19:15:48 +0530

    Delete Assignment 4 directory

--- Assignment 4/Q9.py
+++
@@ -1,7 +0,0 @@
-start = int(input("Enter start: "))
-end = int(input("Enter end: "))
-divisor = int(input("Enter divisor: "))
-
-for i in range(start, end+1):
-    if i % divisor == 0:
-        print(i, end=" ")
