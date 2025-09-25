fd02c9a2ab54cb9cdec732573d4b7dcb419689a1
Sunil Utagi <sunilutagi7@gmail.com>
2025-09-25 19:16:43 +0530

    Delete Assignment 9 directory

--- Assignment 9/Q3.py
+++
@@ -1,9 +0,0 @@
-def reverse_no(num, rev=0):
-    # Base case
-    if num == 0:
-        return rev
-    # Recursive case
-    return reverse_no(num // 10, rev * 10 + num % 10)
-
-n = int(input("Enter number: "))
-print("Reverse:", reverse_no(n))
--- Assignment 9/Q4.py
+++
@@ -1,8 +0,0 @@
-#sum of first n natural numbers 
-def sum_n(n):
-    if n == 0:
-        return 0
-    return n + sum_n(n - 1)
-
-n = int(input("Enter number: "))
-print("Sum of first n natural numbers:", sum_n(n))
